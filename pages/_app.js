6017084664f9a88fce281934c6f67e8295bd1a39
anhle <anh@silentium.io>
2021-04-03 16:58:01 +0700

    Add SEO

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,19 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Haha</title>
+        <title>Bài toán cân 13 viên bi</title>
+        <link rel="shortcut icon" href="/logo.jpeg" type="image/x-icon" />
+        <meta
+          name="description"
+          content="Cho 13 viên bi trong đó có 1 viên bi khác khối lượng (lớn hơn hoặc bé hơn) sử dụng cân cân bằng, trong 3 lần cân tìm được viên bi đó"
+        />
+        <meta property="og:title" content="Bài toán cân 13 viên bi" />
+        <meta
+          property="og:description"
+          content="Cho 13 viên bi trong đó có 1 viên bi khác khối lượng (lớn hơn hoặc bé hơn) sử dụng cân cân bằng, trong 3 lần cân tìm được viên bi đó"
+        />
+        <meta property="og:image" content="/placeholder.png" />
+        <meta property="og:url" content="https://www.lehuuvietanh.me" />
       </Head>
       <Component {...pageProps} />{" "}
     </>
